6d240d00fa4b4fd770b4b25e9e9e79b2782b915b
Jens Willmer <info@jwillmer.de>
2020-07-10 16:32:55 +0200

    Update power_monitor.py

--- scripts/tests/power_monitor.py
+++ scripts/tests/power_monitor.py
@@ -31,20 +31,20 @@ ina4.bus_voltage_range = BusVoltageRange.RANGE_16V
 
 # measure and display loop
 while True:
-    #bus_voltage1 = ina1.bus_voltage        # voltage on V- (load side)
-    #shunt_voltage1 = ina1.shunt_voltage    # voltage between V+ and V- across the shunt
-    #power1 = ina1.power
-    #current1 = ina1.current                # current in mA
-#
-    #bus_voltage2 = ina2.bus_voltage        # voltage on V- (load side)
-    #shunt_voltage2 = ina2.shunt_voltage    # voltage between V+ and V- across the shunt
-    #power2 = ina2.power
-    #current2 = ina2.current                # current in mA
-    #
-    #bus_voltage3 = ina3.bus_voltage        # voltage on V- (load side)
-    #shunt_voltage3 = ina3.shunt_voltage    # voltage between V+ and V- across the shunt
-    #power3 = ina3.power
-    #current3 = ina3.current                # current in mA
+    bus_voltage1 = ina1.bus_voltage        # voltage on V- (load side)
+    shunt_voltage1 = ina1.shunt_voltage    # voltage between V+ and V- across the shunt
+    power1 = ina1.power
+    current1 = ina1.current                # current in mA
+
+    bus_voltage2 = ina2.bus_voltage        # voltage on V- (load side)
+    shunt_voltage2 = ina2.shunt_voltage    # voltage between V+ and V- across the shunt
+    power2 = ina2.power
+    current2 = ina2.current                # current in mA
+    
+    bus_voltage3 = ina3.bus_voltage        # voltage on V- (load side)
+    shunt_voltage3 = ina3.shunt_voltage    # voltage between V+ and V- across the shunt
+    power3 = ina3.power
+    current3 = ina3.current                # current in mA
     
     bus_voltage4 = ina4.bus_voltage        # voltage on V- (load side)
     shunt_voltage4 = ina4.shunt_voltage    # voltage between V+ and V- across the shunt
@@ -52,9 +52,9 @@ while True:
     current4 = ina4.current                # current in mA
     
     # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
-    #print("PSU Voltage:{:6.3f}V    Shunt Voltage:{:9.6f}V    Load Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}A".format((bus_voltage1 + shunt_voltage1),(shunt_voltage1),(bus_voltage1),(power1),(current1/1000)))
-    #print("PSU Voltage:{:6.3f}V    Shunt Voltage:{:9.6f}V    Load Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}A".format((bus_voltage2 + shunt_voltage2),(shunt_voltage2),(bus_voltage2),(power2),(current2/1000)))
-    #print("PSU Voltage:{:6.3f}V    Shunt Voltage:{:9.6f}V    Load Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}A".format((bus_voltage3 + shunt_voltage3),(shunt_voltage3),(bus_voltage3),(power3),(current3/1000)))
+    print("PSU Voltage:{:6.3f}V    Shunt Voltage:{:9.6f}V    Load Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}A".format((bus_voltage1 + shunt_voltage1),(shunt_voltage1),(bus_voltage1),(power1),(current1/1000)))
+    print("PSU Voltage:{:6.3f}V    Shunt Voltage:{:9.6f}V    Load Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}A".format((bus_voltage2 + shunt_voltage2),(shunt_voltage2),(bus_voltage2),(power2),(current2/1000)))
+    print("PSU Voltage:{:6.3f}V    Shunt Voltage:{:9.6f}V    Load Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}A".format((bus_voltage3 + shunt_voltage3),(shunt_voltage3),(bus_voltage3),(power3),(current3/1000)))
     print("PSU Voltage:{:6.3f}V    Shunt Voltage:{:9.6f}V    Load Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}A".format((bus_voltage4 + shunt_voltage4),(shunt_voltage4),(bus_voltage4),(power4),(current4/1000)))
     print("")
     print("")
