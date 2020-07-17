00591e211bf9fee38fc71bcb20ac99e2842d6e64
Jens Willmer <info@jwillmer.de>
2020-07-17 12:23:12 +0200

    refactoring

--- scripts/updateScreen.py
+++ scripts/updateScreen.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'media')
 libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'libraries')
 if os.path.exists(libdir):
     sys.path.append(libdir)
